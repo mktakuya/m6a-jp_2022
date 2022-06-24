abde513fd6bb63722031babb69960c36aa1c5ebc
Takuya Mukohira <takuya.mk96@gmail.com>
2022-06-25 00:16:39 +0900

    Fix @/ import

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tsconfigPaths()],
   resolve: {
     alias: { '@': resolve(__dirname, './src') },
   },
